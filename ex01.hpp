2c08855314dc338e5dc44728a39c889d9ce4e699
BuiniyYarik <sokoslav1707@gmail.com>
2022-03-07 18:30:16 +0400

    Nice

--- ex01.hpp
+++ ex01.hpp
@@ -1,8 +1,8 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <class WhatKind>
-void multiplies(WhatKind& sum, WhatKind x, int n) {
+template <class T>
+void multiplies(T& sum, T x, int n) {
     sum = 1;
     for (int i = 1; i <= n; ++i) {
         sum += i * x;
