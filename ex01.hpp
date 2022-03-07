8a93c3c7935bc799d54bc0c0da072dace34250b8
BuiniyYarik <sokoslav1707@gmail.com>
2022-03-07 18:32:24 +0400

    Nice

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <class T>
-void multiplies(T& sum, T x, int n) {
+void multiples (T& sum, T x, int n) {
     sum = 1;
     for (int i = 1; i <= n; ++i) {
         sum += i * x;
