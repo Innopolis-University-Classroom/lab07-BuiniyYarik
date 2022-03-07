62117065c70f18fc28cd42ed315cea079fc69dcf
BuiniyYarik <sokoslav1707@gmail.com>
2022-03-07 18:25:54 +0400

    Nice

--- ex01.hpp
+++ ex01.hpp
@@ -1,7 +1,7 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <typename WhatKind>
+template <class WhatKind>
 void multiplies(WhatKind& sum, WhatKind x, int n) {
     sum = 1;
     for (int i = 1; i <= n; ++i) {
